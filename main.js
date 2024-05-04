201c3c83801082ae097de60f8858fb25e9fe35ae
Your Name <you@example.com>
2024-05-04 19:19:05 +0300

    three js import

--- main.js
+++ main.js
@@ -1,47 +1,119 @@
-// gsap animation elements  movement
+// Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+//Create a Three.JS Scene
+const scene = new THREE.Scene();
+//create a new camera with positions and angles
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+//Keep track of the mouse position, so we can make the eye move
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+//Keep the 3D object on a global variable so we can access it later
+let object;
+
+//OrbitControls allow the camera to move around the scene
+let controls;
+
+//Set which object to render
+let objToRender = "eye";
+
+//Instantiate a loader for the .gltf file
+const loader = new GLTFLoader();
+
+//Load the file
+loader.load(
+  `models/${objToRender}/scene.gltf`,
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    object = gltf.scene;
+    scene.add(object);
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
+  }
+);
+
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth / 3, window.innerHeight / 4);
+
+//Add the renderer to the DOM
+document.getElementById("second-element").appendChild(renderer.domElement);
+
+//Set how far the camera will be from the 3D model
+camera.position.z = objToRender === "eye" ? 3 : 500;
+
+//Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500); //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(
+  0x333333,
+  objToRender === "eye" ? 5 : 1
+);
+scene.add(ambientLight);
+
+//Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  //Here we could add some code to update the scene, adding some automatic movement
+
+  //Make the eye move
+  if (object && objToRender === "eye") {
+    //I've played with the constants here until it looked good
+    object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
+    object.rotation.x = -1.2 + (mouseY * 2.5) / window.innerHeight;
+  }
+  renderer.render(scene, camera);
+}
+
+//Start the 3D rendering
+animate();
+
+//------------------gsap area start --------------------------------------
+
+// Register the ScrollTrigger plugin
 
 var tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".center",
-    start: "-900 1%",
-    end: "50% 50%",
-    scrub: 1,
+    start: "0 95%",
+    end: "150% 50%",
+    scrub: 2,
     // markers: true,
   },
-  // Callback function to start the animation when the page loads
-  onLoad: function () {
-    tl.play(); // Start the timeline
-  },
 });
-tl.to(
-  "#orange-cut",
-  {
-    top: "40%",
-    left: "40%",
-  },
-  "orange"
-);
-tl.to(
-  "#orange",
-  {
-    width: "15%",
-    top: "40%",
-    right: "30%",
-  },
-  "orange"
-);
-tl.to(
-  "#leaf",
-  {
-    top: "40%",
-    rotate: "130deg",
-    left: "20%",
-  },
-  "orange"
-);
-//-----------------------------------------------------------------------------
 
-//Awesome Mouse Spotlight Effect
+tl.to("#first-element", {
+  top: "60%",
+  left: "23%",
+});
+tl.to("#second-element", {
+  width: "15%",
+  top: "60%",
+  right: "10%",
+});
+tl.to("#third-element", {
+  top: "70%",
+  rotate: "130deg",
+  right: "30%",
+});
 
 const Span = document.querySelector(".hover-btn");
 const OverLay = document.querySelector(".two");
@@ -56,4 +128,8 @@ window.addEventListener("mousemove", (e) => {
     duration: 0.3,
     ease: "sine.out",
   });
+});
+
+Span.addEventListener("click", () => {
+  OverLay.classList.toggle("is-open");
 });
